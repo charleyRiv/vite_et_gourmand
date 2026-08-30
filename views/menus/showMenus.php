367c4936c1bed7529fc67a8ee42d9eae79daf04d
charleyRiv <charley.rivory@gmail.com>
2026-08-30 02:36:26 +0200

    page menu details css

--- views/menus/showMenus.php
+++ views/menus/showMenus.php
@@ -4,55 +4,101 @@
  * @var array $dishes
  * @var int $h1
  * @var array $picture
+ * @var string $heroImage
  */
 require_once __DIR__ . '/../layouts/header.php';
+
+$heroTitle = htmlspecialchars($h1);
+$heroImage    = '/assets/images/uploads/hero_banner_charte_001.jpeg';
+require_once __DIR__ . '/../layouts/hero.php';
 ?>
-<br>
-<main>
-    <h1><?= htmlspecialchars($h1)?></h1>
-    <section>
-        <p><?= htmlspecialchars($menu['description']) ?></p>
-        <p>Thème : <?= htmlspecialchars($menu['theme']) ?></p>
-        <p>Régime : <?= htmlspecialchars($menu['diet']) ?></p>
-        <div>
-            <?php foreach ($dishes as $dish): ?>
-                <article>
-                    <!-- photos -->
-                    <?php if (!empty($dish['picture'])): ?>
-                        <img 
-                            src="<?= $dish['picture']['url'] ?>"
-                            alt="<?= $dish['picture']['alt_text'] ?>"
-                        >
-                    <?php else : ?>
-                        <p>Image indisponible</p>
-                    <?php endif; ?>
 
-                    <!-- infos plat -->
-                    <h3><?= htmlspecialchars($dish['dish_type']) ?></h3>
-                    <p><?= htmlspecialchars($dish['title']) ?></p>
-                    <p><?= htmlspecialchars($dish['description']) ?></p>
+<main class="page-menu-details">
+    <section class="section-menu-details">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 description">
+                    <p><?= htmlspecialchars($menu['description']) ?></p>
+                </div>
+                <div class="row g-0 mb-0 categorie">
+                    <div class="col-12 col-lg-auto">
+                        <p>Thème : <?= htmlspecialchars($menu['theme']) ?></p>
+                    </div>
+                    <div class="col-12 col-lg-auto">
+                        <p>Régime : <?= htmlspecialchars($menu['diet']) ?></p>
+                    </div>
+                </div>
+                <div class="row g-0 mb-0">
+                    <?php foreach ($dishes as $dish): ?>
+                    <div class="col-12 col-lg-4">
+                        <article class="menu-dish">
+                            <!-- photos -->
+                            <div class="col-12">
+                                <?php if (!empty($dish['picture'])): ?>
+                                    <img 
+                                        src="<?= $dish['picture']['url'] ?>"
+                                        alt="<?= $dish['picture']['alt_text'] ?>"
+                                        class="menu-picture-dish"
+                                    >
+                                <?php else : ?>
+                                    <p>Image indisponible</p>
+                                <?php endif; ?>
+                            </div>
+                        
+                            <!-- infos plat -->
+                            <div class="menu-details">
+                                <div class="col-12 mb-3">
+                                    <h3><?= htmlspecialchars($dish['dish_type']) ?></h3>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <p><?= htmlspecialchars($dish['title']) ?></p>
+                                </div>
+                                <div class="col-12 mb-3">
+                                    <p><?= htmlspecialchars($dish['description']) ?></p>
+                                </div>
 
-                    <!-- ajout allergenes -->
-                    <?php if (!empty($dish['allergens'])): ?>
-                        <?php foreach ($dish['allergens'] as $allergen): ?>
-                            <li><?= htmlspecialchars($allergen['label']) ?></li>
-                        <?php endforeach ;?>
-                    <?php endif ;?>
+                                <!-- ajout allergenes -->
+                                <?php if (!empty($dish['allergens'])): ?>
+                                    <div class="col-12 menu-allergen">
+                                        allergene : <?= htmlspecialchars(implode(', ', array_column($dish['allergens'], 'label'))) ?>
+                                    </div>
+                                <?php endif ;?>
+                            </div>
 
-                </article>
-            <?php endforeach ;?>
+                        </article>
+                    </div>
+                    <?php endforeach ;?>
 
+                </div>
+
+                <div class="row g-0 mb-0 menu-infos">
+                    <div class="col-12 col-lg-auto">
+                        <p><?= htmlspecialchars($menu['price_per_person']) ?>€/ personne</p>
+                    </div>
+                    <div class="col-12 col-lg-auto">
+                        <p>Minimum <?= htmlspecialchars($menu['min_persons']) ?> personnes</p>
+                    </div>
+                    <div class="col-12 col-lg-auto">
+                    <p>Stock : <?php if ($menu['remaining_stock'] === 0): ?>
+                                    <span>Bientôt disponible</span>
+                                <?php else: ?>
+                                    <?= htmlspecialchars($menu['remaining_stock']) ?> disponible(s)
+                                <?php endif; ?></p>
+                    </div>
+                </div>
+                <div class="col-12 menu-condition">
+                    <p>Conditions de réservation: <?= htmlspecialchars($menu['conditions']) ?></p>
+                </div>
+                <div class="row g-0 mb-0 menu-commande">
+                    <div class="col-12 col-lg-auto justify-content-flex-end">
+                        <a href="/commande/etape-1?menu_id=<?= $menu['menu_id'] ?>" class="btn btn-primary">Commander</a>
+                    </div>
+                    <div class="col-12 col-lg-auto justify-content-flex-start">
+                        <a href="/menus">Revenir à la liste des menus</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        <p><?= htmlspecialchars($menu['price_per_person']) ?>€/ personne</p>
-        <p>Minimum <?= htmlspecialchars($menu['min_persons']) ?> personnes</p>
-        <p>Stock : <?php if ($menu['remaining_stock'] === 0): ?>
-                        <span>Bientôt disponible</span>
-                    <?php else: ?>
-                        <?= htmlspecialchars($menu['remaining_stock']) ?> disponible(s)
-                    <?php endif; ?></p>
-        <p>Conditions de réservation: <?= htmlspecialchars($menu['conditions']) ?></p>
-        <a href="/commande/etape-1?menu_id=<?= $menu['menu_id'] ?>">Commander</a>
-        <a href="/menus">Revenir à la liste des menus</a>
     </section>
 </main>
 
