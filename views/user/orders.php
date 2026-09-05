5fa8c0d3c939b6d3770cb0fac9e2b4136e31361c
charleyRiv <charley.rivory@gmail.com>
2026-09-06 01:25:42 +0200

    page client order clen post review button

--- views/user/orders.php
+++ views/user/orders.php
@@ -125,7 +125,7 @@ require_once __DIR__ . '/../layouts/header.php';
                         </div>
                     </fieldset>
                         
-                        <?php if ($order['current_status'] === 'completed' ) : ?> <!-- && !empty($review)) : ?--> 
+                        <?php if ($order['current_status'] === 'completed' && !empty($review)) : ?> 
                             <div class="col-12 post">
                                 <a href="/mon-espace/commande/<?= $order['order_id']?>/avis" class="btn btn-primary">Poster un avis</a>
                             </div> 
