1957e30699aba804514d868e2bdfc17b19adfa98
charleyRiv <charley.rivory@gmail.com>
2026-09-09 01:41:18 +0200

    page employee orders history css jscript

--- views/employee/orderHistory.php
+++ views/employee/orderHistory.php
@@ -9,45 +9,69 @@
 require_once __DIR__ . '/../layouts/header.php';
 ?>
 
-<br>
-<main>
-    <h1><?=  htmlspecialchars($h1)?></h1>
-    <section>
-        <h3>Date de la prestation</h3>
-        <p><?= htmlspecialchars($order['event_date'])?> à <?= htmlspecialchars($order['delivery_time'])?></p>
+<main class="page-employee-order-history">
+    <h2><?=  htmlspecialchars($h1)?></h2>
+    <section class="section-employee-order-info">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 title">
+                    <h3>Date de la prestation</h3>
+                </div>
+
+                <div class="col-12">
+                    <?= htmlspecialchars($order['event_date'])?> à <?= htmlspecialchars($order['delivery_time'])?>
+                </div>        
+            </div>
+        </div>
     </section>
 
-    <h3>Historique</h3>
-    <?php foreach ($statuses as $status): ?>
-    <section>
-        <table>
-            <tr>
-                <th>status</th>
-                <td><?= htmlspecialchars($status['status_fr'] ?? '')?></td>
-            </tr>
-            <tr>
-                <th>date</th>
-                <td><?= htmlspecialchars($status['modified_at'] ?? '')?></td>
-            </tr>
-            <?php if ($status['status'] === 'cancelled') : ?>
-            <tr>
-                <td>mode de contact</td>
-                <td><?= htmlspecialchars($status['contact_mode'] ?? '')?></td>
-            </tr>
-            <tr>
-                <td>motif</td>
-                <td><?= htmlspecialchars($status['reason'] ?? '')?></td>
-            </tr>
-            <?php endif ?>
-        </table>
-    <br>
-    </section>
-    <?php endforeach; ?>
+    <section class="section-employee-order-history">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 title">
+                    <h3>Historique</h3>
+                </div>
 
-    <a href="<?= $basePath ?>/commandes">Retour aux commandes</a>
+                <?php foreach ($statuses as $status): ?>
+                    <fieldset>
+                        <table class="table table-borderless">
+                            <tr>
+                                <th>status</th>
+                                <td>
+                                    <span class="btn <?= getStatusClass($status['status']) ?>">
+                                        <?= htmlspecialchars($status['status_fr'] ?? '')?>
+                                    </span>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th>date</th>
+                                <td><?= htmlspecialchars($status['modified_at'] ?? '')?></td>
+                            </tr>
+                            <?php if ($status['status'] === 'cancelled') : ?>
+                            <tr>
+                                <td>mode de contact</td>
+                                <td><?= htmlspecialchars($status['contact_mode'] ?? '')?></td>
+                            </tr>
+                            <tr>
+                                <td>motif</td>
+                                <td><?= htmlspecialchars($status['reason'] ?? '')?></td>
+                            </tr>
+                            <?php endif ?>
+                        </table>
+                    </fieldset>    
+                <?php endforeach; ?>
+                
+                <div class="col-12 back">
+                    <a href="<?= $basePath ?>/commandes">Retour aux commandes</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
 </main>
-<br>
+
 
 <?php
-//require_once __DIR__ . '/../layouts/footer.php';
+require_once __DIR__ . '/../layouts/footer.php';
 ?>
