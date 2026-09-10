1cf2756b6e0d98aa4b01e91b12b7b0f193150143
charleyRiv <charley.rivory@gmail.com>
2026-09-10 18:09:55 +0200

    page 403 et 404 css

--- views/errors/403.php
+++ views/errors/403.php
@@ -2,10 +2,38 @@
 require_once __DIR__ . '/../layouts/header.php';
 ?>
 
-<main>
-    <h1>Vous n’êtes pas sur la liste des invités!</h1>
-    <a href="/connexion">Me connecter</a>
-    <a href="/">Retour à l'accueil</a>
+<main class="page-403">
+    <section class="section-403">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Vous n’êtes pas sur la liste des invités!</h2>
+                </div>
+                <div class="col-12 image-grp">
+                    <img src="/assets/images/uploads/403_error_001.png" 
+                    alt="un personnage en dessin vectorielle habillé en tenu de cuisinier, 
+                    l'air embarrassé, soulève une cloche et rélève une assiette vide">
+                    <div class="image-text">
+                        Cet espace est réservé à notre équipe. Comme nos menus secrets, 
+                        certaines choses ne sont pas pour tout le monde... du moins pas encore !
+                    </div>
+                </div>
+                <div class="col-auto">
+                    Si vous êtes un client, connectez-vous à votre espace personnel. 
+                    Si vous êtes un employé ou un administrateur, vérifiez vos identifiants 
+                    de connexion.
+                </div>
+
+                <div class="col">
+                    <a href="/connexion" class="btn btn-primary">Me connecter</a>
+                </div>
+
+                <div class="'col">
+                    <a href="/" class="btn btn-primary">Retour à l'accueil</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
--- views/errors/404.php
+++ views/errors/404.php
@@ -2,10 +2,36 @@
 require_once __DIR__ . '/../layouts/header.php';
 ?>
 
-<main>
-    <h1>Oups... Cette page s’est éclipsée!</h1>
-    <a href="/menus">Voir nos menus</a>
-    <a href="/">Retour à l'accueil</a>
+<main class="page-404">
+    <section class="section-404">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Oups... Cette page s’est éclipsée!</h2>
+                </div>
+                <div class="col-12 image-grp">
+                    <img src="/assets/images/uploads/404_error_002.png" 
+                    alt="un personnage en dessin vectorielle habillé en tenu de cuisinier, 
+                    l'air embarrassé, soulève une cloche et rélève une assiette vide">
+                    <div class="image-text">
+                        On a cherché partout, même dans nos recettes secrètes, mais la page que vous demandez est introuvable
+                    </div>
+                </div>
+                <div class="col-auto">
+                    Pas de panique ! Contrairement à nos menus, cette page ne sera pas livrée. <br>
+                    Mais nos plats, eux, sont toujours au rendez-vous.
+                </div>
+
+                <div class="col">
+                    <a href="/menus" class="btn btn-primary">Voir nos menus</a>
+                </div>
+
+                <div class="'col">
+                    <a href="/" class="btn btn-primary">Retour à l'accueil</a>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
